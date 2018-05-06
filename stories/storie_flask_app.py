468f99da299d38c57bafc23d4d716d9aa0c057d2
Guille Muriel <guille.siesta.muriel@gmail.com>
2018-05-06 23:42:35 +0200

    añadida parte login muy muy basica

--- stories/storie_flask_app.py
+++ stories/storie_flask_app.py
@@ -5,13 +5,35 @@ import json
 import os
 from storie import Storie
 from first_storie import firststorie
+from flask import Flask, render_template, session, redirect, url_for, escape
+from flask_session import Session
 
 app = Flask(__name__)
 
 
 @app.route('/')  # aquí añadir un WELCOME y que haga o signup o signin
 def s():
-    return jsonify(status="OK")
+    return render_template('login.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        session['username'] = username
+        return redirect(url_for('index'))
+    else:
+        return redirect(url_for('error'))
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/error')
+def error():
+    return jsonify(status="CHUNGO LOCO")
 
 
 @app.route('/status')
@@ -25,10 +47,6 @@ def storie():
     return jsonify(solucion=firststorie.storie, pista1=firststorie.clue[0], pista2=firststorie.clue[1], pista3=firststorie.clue[2], storie=firststorie.sh_storie, title=firststorie.title)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    return jsonify(status="OK")
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+    app.run(host='0.0.0.0', port=5000, debug=True)  # a la hora de desplegarlo cambiar a puerto 80
