1106b81c29c4e019b771e1f0bbb7802b884b2ae3
Guille Muriel <guille.siesta.muriel@gmail.com>
2018-08-22 20:46:16 +0200

    cambio de variables

--- stories/storie_flask_app.py
+++ stories/storie_flask_app.py
@@ -53,9 +53,9 @@ authenticate("hobby-aemopclnoaicgbkedjdpdnbl.dbs.graphenedb.com:24780", user, pa
 graph = Graph("hobby-aemopclnoaicgbkedjdpdnbl.dbs.graphenedb.com:24780", bolt = False)
 
 graphenedb_url = os.environ.get("GRAPHENEDB_BOLT_URL")
-graphenedb_user = os.environ.get("GRAPHENEDB_BOLT_USER")
-graphenedb_pass = os.environ.get("GRAPHENEDB_BOLT_PASSWORD")
-graph = Graph("hobby-aemopclnoaicgbkedjdpdnbl.dbs.graphenedb.com:24780", user="app106476859-QJbr1C", password="b.M2enCUumZgWh.d4zbtjAHquDM6pv5", bolt = True, secure = True, http_port = 24789, https_port = 24780)
+graphenedb_user = os.environ.get("app106476859-QJbr1C")
+graphenedb_pass = os.environ.get("b.M2enCUumZgWh.d4zbtjAHquDM6pv5")
+graph = Graph("hobby-aemopclnoaicgbkedjdpdnbl.dbs.graphenedb.com:24780", user=graphenedb_user, password=graphenedb_pass, bolt = True, secure = True, http_port = 24789, https_port = 24780)
 
 # app.config.from_object(__name__)
 
